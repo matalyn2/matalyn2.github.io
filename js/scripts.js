6d2046852b0ffe1ecdee3b70681b7c41e597ce63
Matalyn Talbot <matalyn.nicole@gmail.com>
2022-11-28 13:45:10 -0700

    Testing API

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,25 @@
 document.getElementById('lastModified') = document.lastModified;
 document.getElementById('date') = new Date().getFullYear;
+
+const url = "https://www.fruityvice.com/api/fruit/all"
+// apiFetch function to get JSON information
+async function apiFetch() {
+  try {
+    const response = await fetch(url, {
+        mode: 'no-cors', 
+        credentials: 'same-origin', 
+        headers: {
+            'origin': 'https://localhost:8888'},});
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data); // had to uncomment
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
